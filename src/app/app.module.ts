1465b5eb254761b84cb370175d3f3688998db803
Martin <mar.gardemann@gmail.com>
2018-02-27 15:07:45 +0100

    fe-auth start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-
+// -- components
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
+// -- guards
+import { RequireAnonGuardService } from './guards/require-anon-guard.service';
+import { RequireUserGuardService } from './guards/require-user-guard.service';
+import { InitAuthGuardService } from './guards/init-auth-guard.service';
+
+// -- pages
+import { HomepageComponent } from './pages/homepage/homepage.component';
+
+// routes
+const routes: Routes = [
+  { path: '',  component: HomepageComponent, canActivate: [ InitAuthGuardService ] },
+ // { path: 'login',  component: AuthLoginPageComponent, canActivate: [ RequireAnonGuardService ] },
+ // { path: 'signup',  component: AuthSignupPageComponent, canActivate: [ RequireAnonGuardService ] },
+ // { path: 'page',  component: ... , canActivate: [ RequireUserGuardService ] },
+ // { path: '**', redirectTo: '' }
+];
+
+// don't forget to register the guards (and the AuthService) in the providers
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  title = 'app';
+  loading = true;
+  anon: boolean;
+  user: any;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.authService.userChange$.subscribe((user) => {
+      this.loading = false;
+      this.user = user;
+      this.anon = !user;
+    });
+  }
+
+  logout() {
+    this.authService.logout()
+      .then(() => this.router.navigate(['/login']));
+  }
+}
+
+
